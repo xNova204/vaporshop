52edf135d498b6fce566a34840f14ea273ba6d00
Nova <x.nova.204@gmail.com>
2024-12-16 21:17:17 +0200

    I beg

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded correctly
+  base: '/vaporshop/', // Ensures assets are loaded correctly
   build: {
     outDir: 'dist',
     sourcemap: true
