99708d62f38fc55c491ac84c3a3e55a1bac3ce91
elisa <n.elisa204@gmail.com>
2025-12-10 07:37:05 +0200

    Updated genre select and nicer looking games

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'vaporshop',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'vaporshop',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
